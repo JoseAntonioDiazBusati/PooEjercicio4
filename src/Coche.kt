e1075732276758f179b74cabac348ebbf1c6bd2d
JoseAntonio <jdiabus511@g.educaand.es>
2025-01-20 20:53:44 +0100

    ejercicio4

--- src/Coche.kt
+++ src/Coche.kt
@@ -1,12 +1,8 @@
-class Coche (var color: String, val marca: String, val modelo:String, val numCaballos: Int, val numPuertas: Int, val matricula: String) {
-
+class Coche (var color: String?, val marca: String?, val modelo:String?, val numCaballos: Int?, val numPuertas: Int?, val matricula: String?) {
 
 
     init{
-        require(marca.isNotBlank()){"La marca del coche no puede estar vacia o ser nula."}
-        require(modelo.isNotBlank()){"El modelo del coche no puede estar vacío o ser nulo."}
-        require(matricula.isNotBlank()){}
-        require(color.isNotBlank()){}
+
     }
 
     override fun toString(): String {
