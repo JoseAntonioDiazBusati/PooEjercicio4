9b8b057a53aeb2d04f160929ee93b9b062e42be1
JoseAntonio <jdiabus511@g.educaand.es>
2025-01-20 21:16:06 +0100

    Ejercicio4 primera parte

--- src/Coche.kt
+++ src/Coche.kt
@@ -2,8 +2,15 @@ class Coche (var color: String?, val marca: String?, val modelo:String?, val num
 
 
     init{
-
+        require(!marca.isNullOrEmpty()) { "La marca no puede ser nulo o vacío.  " }
+        require(!modelo.isNullOrEmpty()) { "El modelo no puede ser nulo o vacío." }
+        require(!color.isNullOrBlank()) { "El color nu puede estar vacío." }
+        require(!matricula.isNullOrEmpty()) { "La matricula no puede ser nulo." }
+        require(numCaballos != null)  { "Los numeros de caballos no pueden ser nulos" }
+        require(numPuertas != null)  { "El numero de puertas no puede ser nulo" }
     }
+
+    constructor(){}
 
     override fun toString(): String {
         return "El coche es de color $color, de la marca $marca, modelo $modelo, con una potencia de $numCaballos, con $numPuertas puertas y cuya matricula es: $matricula"
